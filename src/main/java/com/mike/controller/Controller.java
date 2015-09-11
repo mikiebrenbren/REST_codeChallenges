6385eea1886d7ae7389f68262eef8e277d01f672
Michael Brennan <Michael Brennan>
2015-09-11 12:05:12 -0600

    finished with the intersection endpoint, service logic and tests

--- src/main/java/com/mike/controller/Controller.java
+++ src/main/java/com/mike/controller/Controller.java
@@ -1,23 +1,24 @@
 package com.mike.controller;
 
 import com.mike.model.BuzzModel;
+import com.mike.model.IntersectionModel;
 import com.mike.model.ReverseModel;
+import com.mike.model.request.InputWrapper;
 import com.mike.service.BuzzService;
+import com.mike.service.IntersectionService;
 import com.mike.service.ReverseService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
- * Created by michaelbrennan on 9/10/15.
+ * Created by Michael Brennan on 9/10/15.
  */
 @RestController
 @RequestMapping("/")
@@ -31,6 +32,9 @@ public class Controller {
     @Autowired
     ReverseService reverseService;
 
+    @Autowired
+    IntersectionService intersectionService;
+
     @RequestMapping("buzz")
     public ResponseEntity<BuzzModel> buzz(HttpServletRequest request, HttpServletResponse response){
         return new ResponseEntity<>(buzzService.buzz(), HttpStatus.OK);
@@ -41,6 +45,13 @@ public class Controller {
         return new ResponseEntity<>(reverseService.reverse(s), HttpStatus.OK);
     }
 
+    @RequestMapping(value = "intersection", method = RequestMethod.POST)
+    public ResponseEntity<IntersectionModel> intersection(HttpServletRequest request, HttpServletResponse response, @RequestBody InputWrapper inputWrapper){
+        if(inputWrapper.getArr1().size() != 2){
+            return new ResponseEntity<>(new IntersectionModel(new ArrayList<>()), HttpStatus.BAD_REQUEST);  //must be two arrays to compare
+        }
+        return new ResponseEntity<>(intersectionService.getIntersection(inputWrapper.getArr1().get(0), inputWrapper.getArr1().get(1)), HttpStatus.OK);
+    }
 
 
 }
--- src/main/java/com/mike/service/IntersectionService.java
+++ src/main/java/com/mike/service/IntersectionService.java
@@ -1,22 +1,49 @@
 package com.mike.service;
 
+import com.mike.model.IntersectionModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * Created by michaelbrennan on 9/11/15.
+ * Created by Michael Brennan on 9/11/15.
+ * Find the intersection of two arrays, like {1,3,5,7,9}, {1,2,3,4,5}; the intersection is {1,3,5}
  */
 @Service
 public class IntersectionService {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-    public int[] getIntersection(int[] arr1, int[] arr2){
+    public IntersectionModel getIntersection(Integer[] arr1, Integer[] arr2){
+        if (arr1.length == 0 || arr2.length == 0) {
+            logger.debug("Empty array: no intersection data");
+            return new IntersectionModel(new ArrayList<>());
+        }
+        return new IntersectionModel(arr1.length <= arr2.length ?intersectionOf(arr1, arr2):intersectionOf(arr2, arr1));
+    }
 
-
-
-        return null;
+    private List<Integer> intersectionOf(Integer[]smaller, Integer[]bigger){
+        List<Integer> intersection = new ArrayList<>();
+        List<Integer> bigList =
+                Arrays.stream(bigger)
+                .collect(Collectors.toList());
+        for (int i : smaller){
+            if (bigList.contains(i)){
+                if (!intersection.contains(i)){ //only add number if it is not already contained
+                    logger.debug("Intersectional Data: " + i);
+                    intersection.add(i);
+                }
+            }
+        }
+        if (intersection.isEmpty()){
+            logger.debug("No intersecting data was found");
+        }
+        return intersection;
     }
 
     public static void main(String[] args) {
